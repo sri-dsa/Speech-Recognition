6b68c5489417ff1addced5dd3f49edec596438d2
Srivathsan Karthikeyan <49575170+sri-dsa@users.noreply.github.com>
2025-07-30 23:37:06 +0100

    Add files via upload

---
+++ speechvalley/feature/wsj/__init__.py
@@ -0,0 +1,6 @@
+
+
+from speechvalley.feature.wsj.extract_wsj import extract 
+from speechvalley.feature.wsj.rename_wsj import renameCD
+from speechvalley.feature.wsj.split_data_by_s5 import split_data_by_s5
+from speechvalley.feature.wsj.wsj_preprocess import wav2feature
---
+++ speechvalley/feature/wsj/extract_wsj.py
@@ -0,0 +1,13 @@
+
+
+import os
+import subprocess
+
+def extract(rootdir):
+  for subdir, dirs, files in os.walk(rootdir):
+    for f in files:
+      if f.endswith('.zip'):
+        fullFilename = os.path.join(rootdir, f)
+        subprocess.call(['atool', '-x', fullFilename])
+        print(f)
+
