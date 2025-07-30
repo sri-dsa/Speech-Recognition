26e5b66dc783021c48759c1d1c8416ea98dc9e58
Srivathsan Karthikeyan <49575170+sri-dsa@users.noreply.github.com>
2025-07-30 23:23:59 +0100

    Add files via upload

---
+++ speechvalley/models/__init__.py
@@ -0,0 +1,5 @@
+
+
+from speechvalley.models.dynamic_brnn import DBiRNN 
+from speechvalley.models.deepSpeech2 import DeepSpeech2
+from speechvalley.models.capsuleNetwork import CapsuleLayer, CapsuleNetwork
